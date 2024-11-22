ecac8c67d0f4451547f33290f349a24a438c2da5
SivaPrasadReddyMitta <S3279394@live.tees.ac.uk>
2024-11-22 12:12:34 +0000

    Commit 7

--- app/src/main/java/uk/ac/tees/mad/univid/screens/ReportScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/univid/screens/ReportScreen.kt
@@ -1,61 +1,207 @@
 package uk.ac.tees.mad.univid.screens
 
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
 import android.net.Uri
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import uk.ac.tees.mad.univid.MainViewModel
 import uk.ac.tees.mad.univid.R
+import uk.ac.tees.mad.univid.Utils.darkPurpleColor
+import uk.ac.tees.mad.univid.Utils.lightPurpleColor
+import uk.ac.tees.mad.univid.ui.theme.poppins
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
+import java.util.Objects
 
 
-
-@OptIn(ExperimentalPermissionsApi::class)
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ReportScreen(navController: NavController, vm : MainViewModel) {
-    val image = remember {
-        mutableStateOf<Uri?>(null)
+    var title by remember {
+        mutableStateOf("")
+    }
+    var description by remember {
+        mutableStateOf("")
+    }
+    var location by remember {
+        mutableStateOf("")
     }
     val context = LocalContext.current
-    Scaffold {
+    val file = context.createImageFile()
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        context.packageName + ".provider", file
+    )
+
+    val cameraLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()){
+        capturedImageUri = uri
+    }
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){
+        if (it)
+        {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        }
+        else
+        {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    Scaffold(modifier = Modifier
+        .fillMaxSize()
+        .windowInsetsPadding(WindowInsets.navigationBars),
+        bottomBar = {
+            BottomNavBar(selectedItem = BottomNavItems.ReportScreen, navController = navController)
+        }
+    ) {
         Column(modifier = Modifier
             .fillMaxSize()
-            .padding(it)) {
-            image.let {
-                AsyncImage(model =image, contentDescription =null)
-            }.run { 
-                Image(painter = painterResource(id = R.drawable.app_logo), contentDescription = null)
+            .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Spacer(modifier = Modifier.height(20.dp))
+            if (capturedImageUri.path?.isNotEmpty() == true)
+            {
+                Image(
+                    modifier = Modifier
+                        .size(300.dp)
+                        .padding(16.dp, 8.dp)
+                        .clip(RoundedCornerShape(30.dp)),
+                    painter = rememberImagePainter(capturedImageUri),
+                    contentDescription = null
+                )
             }
-            Button(onClick = { /*TODO*/ }) {
+            else
+            {
+                Image(
+                    modifier = Modifier
+                        .size(300.dp)
+                        .padding(16.dp, 8.dp)
+                        .clip(RoundedCornerShape(30.dp)),
+                    painter = painterResource(id = R.drawable.app_logo),
+                    contentDescription = null
+                )
+            }
+            Button(onClick = { val permissionCheckResult =
+                ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+
+                if (permissionCheckResult == PackageManager.PERMISSION_GRANTED)
+                {
+                    cameraLauncher.launch(uri)
+                }
+                else
+                {
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+            }, colors = ButtonDefaults.buttonColors(darkPurpleColor)) {
                 Text(text = "Capture Image")
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Column {
+                Text(text = "TITLE", fontFamily = poppins, fontWeight = FontWeight.Bold)
+                TextField(value = title, onValueChange = { title = it },singleLine = true,
+                    colors = TextFieldDefaults.textFieldColors(
+                        disabledTextColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        cursorColor = Color.Black
+                    ))
+            }
+            Spacer(modifier = Modifier.height(25.dp))
+            Column {
+                Text(text = "DESCRIPTION", fontFamily = poppins, fontWeight = FontWeight.Bold)
+                TextField(value = description, onValueChange = { description = it },
+                    colors = TextFieldDefaults.textFieldColors(
+                        disabledTextColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        cursorColor = Color.Black
+                    ))
+            }
+            Spacer(modifier = Modifier.height(25.dp))
+            Column {
+                Text(text = "LOCATION", fontFamily = poppins, fontWeight = FontWeight.Bold)
+                TextField(value = location, onValueChange = { location = it },singleLine = true,
+                    colors = TextFieldDefaults.textFieldColors(
+                        disabledTextColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        cursorColor = Color.Black
+                    ))
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(darkPurpleColor)) {
+                Text(text = "Submit")
             }
         }
     }
 }
 
-private fun createImageUri(context: Context): Uri? {
-    val image = File(context.filesDir, "camera_photo.png")
-    return FileProvider.getUriForFile(context, "uk.ac.tees.mad.univid.provider", image)
+fun Context.createImageFile(): File {
+    val timeStamp = SimpleDateFormat("yyyy_MM_dd_HH:mm:ss").format(Date())
+    val imageFileName = "JPEG_" + timeStamp + "_"
+    val image = File.createTempFile(
+        imageFileName,
+        ".jpg",
+        externalCacheDir
+    )
+
+    return image
 }
 
 fun getAddressFromLocation(context: Context, location: Location, onChange: (String) -> Unit) {
